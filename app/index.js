2e0790dcc17cab1e836af9206c59d495fa70faf3
riyaz04k <riyaz04k@gmail.com>
2025-09-04 18:14:08 +0000

    v2

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+<<<<<<< HEAD
 app.get('/', (req, res) => res.send('Hello from CI/CD pipeline on AWS EC2! i am riyaz khan'));
+=======
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline on AWS EC2! from riyaz'));
+>>>>>>> 934be85 (v2)
 app.listen(port, () => console.log(`App running on port ${port}`));

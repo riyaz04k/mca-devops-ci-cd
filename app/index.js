bd7527c3932ec3410c2def614f029cdd39fff011
riyaz04k <riyaz04k@gmail.com>
2025-11-21 09:50:04 +0000

    Describe changes

--- app/index.js
+++ app/index.js
@@ -125,7 +125,7 @@ function renderStore(productsToShow) {
       </head>
       <body>
         <div class="navbar">
-          <h1>🛒Online Store</h1>
+          <h1>🛒Online Stores</h1>
           <form action="/search" method="get" class="search-bar">
             <input type="text" name="q" placeholder="Search for products...">
             <button type="submit">🔍</button>
